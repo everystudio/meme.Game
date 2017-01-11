0a4b958e9866f0abaa6854738a4f1255e7a8f3ce
watanabe.everystudio <watanabe.everystudio@gmail.com>
2017-01-11 23:36:39 +0900

    とりあえずバナー広告を表示 縦横切り替えでもう少しうまいこと制御したい

---
+++ source/AdsBanner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdsBanner : MonoBehaviour {
+
+#if UNITY_EDITOR
+	string adUnitId1 = "unused";
+#elif UNITY_ANDROID
+    string adUnitId1 = "ca-app-pub-5869235725006697/4481993169";
+#elif UNITY_IPHONE
+    string adUnitId1 = "ca-app-pub-5869235725006697/5958726364";
+#else
+    string adUnitId1 = "unexpected_platform";
+#endif
+	BannerView view1 = null;        // 画面上部
+
+	// Use this for initialization
+	void Start () {
+		BannerView bannerView1 = new BannerView(adUnitId1, AdSize.Banner, AdPosition.Bottom);
+		// Create an empty ad request.
+		AdRequest request1 = new AdRequest.Builder().Build();
+		// Load the banner with the request.
+		bannerView1.LoadAd(request1);
+		view1 = bannerView1;
+		view1.OnAdLoaded += View_OnAdLoaded;
+	}
+	private void View_OnAdLoaded(object sender, System.EventArgs e)
+	{
+		//refreshAds(m_strSceneNow);
+		// 何かしたいならここ
+		//view1.Hide();
+		//view1.Show();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ source/AdsInterstitial.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdsInterstential : MonoBehaviour {
+#if UNITY_EDITOR
+	string adUnitId1 = "unused";
+#elif UNITY_ANDROID
+    string adUnitId1 = "ca-app-pub-5869235725006697/4481993169";
+#elif UNITY_IPHONE
+    string adUnitId1 = "ca-app-pub-5869235725006697/5958726364";
+#else
+    string adUnitId1 = "unexpected_platform";
+#endif
+	InterstitialAd interstitial;
+	private AdRequest request;
+	// Use this for initialization
+	void Start () {
+		interstitial = new InterstitialAd(adUnitId1);
+
+		request = new AdRequest.Builder().Build();
+		interstitial.LoadAd(request);
+
+		interstitial.OnAdClosed += HandleAdClosed;
+	}
+
+	// インタースティシャル広告を閉じた時に走る
+	void HandleAdClosed(object sender, System.EventArgs e)
+	{
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
