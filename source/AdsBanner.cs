7e69967f81341ef913c8f48313e3db63c5c6b443
watanabe.everystudio <watanabe.everystudio@gmail.com>
2017-01-12 23:06:12 +0900

    フッター関連の縦横切り替えを実装 広告との兼ね合いの関係で横画面版がボタン小さい

--- source/AdsBanner.cs
+++ source/AdsBanner.cs
@@ -6,25 +6,75 @@ using GoogleMobileAds.Api;
 public class AdsBanner : MonoBehaviour {
 
 #if UNITY_EDITOR
-	string adUnitId1 = "unused";
+	string adUnitIdTate = "unused";
+	string adUnitIdYoko = "unused";
 #elif UNITY_ANDROID
-    string adUnitId1 = "ca-app-pub-5869235725006697/4481993169";
+    string adUnitIdTate = "ca-app-pub-5869235725006697/4481993169";
+	string adUnitIdYoko = "ca-app-pub-5869235725006697/2917861565";
 #elif UNITY_IPHONE
-    string adUnitId1 = "ca-app-pub-5869235725006697/5958726364";
+    string adUnitIdTate = "ca-app-pub-5869235725006697/5958726364";
+	string adUnitIdYoko = "ca-app-pub-5869235725006697/1301527568";
 #else
-    string adUnitId1 = "unexpected_platform";
+    string adUnitIdTate = "unexpected_platform";
+	string adUnitIdYoko = "unused";
 #endif
-	BannerView view1 = null;        // 画面上部
+	BannerView viewTate = null;        // 縦画面
+	BannerView viewYoko = null;        // 横画面
+
+	void showAdBanner(DeviceOrientationDetector.ORIENTATION _orientation)
+	{
+		if( _orientation == DeviceOrientationDetector.ORIENTATION.TATE)
+		{
+			if( viewYoko!= null)
+			{
+				viewYoko.Hide();
+			}
+			if (viewTate == null)
+			{
+				BannerView bannerViewTate = new BannerView(adUnitIdTate, AdSize.Banner, AdPosition.Bottom);
+				// Create an empty ad request.
+				AdRequest requestTate = new AdRequest.Builder().Build();
+				// Load the banner with the request.
+				bannerViewTate.LoadAd(requestTate);
+				viewTate = bannerViewTate;
+				viewTate.OnAdLoaded += View_OnAdLoaded;
+			}
+			else
+			{
+				viewTate.Show();
+			}
+		}
+		else
+		{
+			if (viewTate != null)
+			{
+				viewTate.Hide();
+			}
+			if (viewYoko == null)
+			{
+				BannerView bannerViewYoko = new BannerView(adUnitIdYoko, AdSize.Banner, AdPosition.BottomLeft);
+				// Create an empty ad request.
+				AdRequest requestYoko = new AdRequest.Builder().Build();
+				// Load the banner with the request.
+				bannerViewYoko.LoadAd(requestYoko);
+				viewYoko = bannerViewYoko;
+				viewYoko.OnAdLoaded += View_OnAdLoaded;
+			}
+			else
+			{
+				viewYoko.Show();
+			}
+		}
+	}	
 
 	// Use this for initialization
 	void Start () {
-		BannerView bannerView1 = new BannerView(adUnitId1, AdSize.Banner, AdPosition.Bottom);
-		// Create an empty ad request.
-		AdRequest request1 = new AdRequest.Builder().Build();
-		// Load the banner with the request.
-		bannerView1.LoadAd(request1);
-		view1 = bannerView1;
-		view1.OnAdLoaded += View_OnAdLoaded;
+		DeviceOrientationDetector.Instance.OnChangeOrientation.AddListener(OnChangedOrientation);
+	}
+
+	private void OnChangedOrientation(DeviceOrientationDetector.ORIENTATION _orientation)
+	{
+		showAdBanner(_orientation);
 	}
 	private void View_OnAdLoaded(object sender, System.EventArgs e)
 	{
